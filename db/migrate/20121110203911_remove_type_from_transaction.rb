b3693f773c6cf8bb0ea69f0dcb50e8b33fb1bf4f
lyubent <ltodorov@dundee.ac.uk>
2012-11-10 23:25:51 +0000

    fixin stuff

--- db/migrate/20121110203911_remove_type_from_transaction.rb
+++
@@ -1,9 +0,0 @@
-class RemoveTypeFromTransaction < ActiveRecord::Migration
-  def up
-    remove_column :transactions, :type
-  end
-
-  def down
-    add_column :transactions, :type, :string
-  end
-end
