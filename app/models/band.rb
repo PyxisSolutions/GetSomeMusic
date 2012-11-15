0a7794a5372050d616d7bdd21285ffede48258cb
lyubent <ltodorov@dundee.ac.uk>
2012-11-15 21:59:32 +0000

    time_to_s = no

--- app/models/band.rb
+++ app/models/band.rb
@@ -11,7 +11,7 @@ class Band < ActiveRecord::Base
     # to use: <%= track.band.has_subscription %>
     if !band.subscription.nil?
       if band.subscription.expires > Date.today.to_time
-        puts 'SUBEXP' + band.subscription.expires
+        puts band.subscription.expires.to_time
         puts 'DATETODAY' + Date.today.to_time
         puts (band.subscription.expires > Date.today.to_time)
         
